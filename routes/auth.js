79b014e0b332b23e260279a5b2f0334e8a58032b
YouSsef <youssef.elhaddady@usmba.ac.ma>
2020-12-04 18:30:21 +0100

    data validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//Validation
+const { registerValidation, loginValidation } = require('./validation');
+
 router.post('/register', async (req, res) => {
+  //Data validation
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //checking if the user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already exists');
+
+  // Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
