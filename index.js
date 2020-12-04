faa052addaa394e0b4aa64ab3aa7071893a6354e
YouSsef <youssef.elhaddady@usmba.ac.ma>
2020-12-04 17:40:49 +0100

    DB connection with mongoose

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // Import Routes
 const authRoute = require('./routes/auth');
 
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () =>
+  console.log('connected to DB!')
+);
+
+// Middleware
+app.use(express.json());
+
+//Route Middlewares
 app.use('/api/user', authRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
